64f9b2eae5d3541ed10d5d37cc2389e6f30bda03
Rlrken <raprap.lariz@gmail.com>
2025-10-01 14:17:06 +0800

    fix vercel

---
+++ minikit.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "@coinbase/onchainkit/minikit";
+
+export default defineConfig({
+  name: "Co-Mission", // Your app name
+  url: "https://localhost:3000", // Change to your deployed URL later
+  description: "My first Base mini app with OnchainKit",
+  icon: "https://yourdomain.com/icon.png", // optional, replace with your logo
+});
---
+++ test-env.js
@@ -0,0 +1,3 @@
+import 'dotenv/config';
+
+console.log("API Key:", process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY);
